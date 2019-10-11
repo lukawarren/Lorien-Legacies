c0445c4a93432e0dfbbf22e8908cdc3b56d8b817
Luka Warren <lukawarren@icloud.com>
2019-10-11 07:52:03 +0100

    stuff

--- java/lorien/legacies/gui/LegacyGui.java
+++ java/lorien/legacies/gui/LegacyGui.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.lwjgl.util.Color;
 
+import lorien.legacies.legacies.levels.LegacyLevels;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiLabel;
@@ -16,22 +17,27 @@ public class LegacyGui extends GuiScreen
 	private static final int YELLOW = 0xE48700;
 	
 	private GuiLabel levelUpLabel;
+	private GuiLabel levelUpLabel2;
 	private GuiButton testButton;
+	
+	private int legacyIndex;
 	
 	@Override
 	public void initGui()
 	{
 		super.initGui();
 		
-		levelUpLabel = new GuiLabel(fontRenderer, 1, this.width / 2 - 75, 0, 15, 75, YELLOW);
+		levelUpLabel = new GuiLabel(fontRenderer, 1, this.width / 2 - 75, 0, 15, 75, 0xFFFFFFFF);
 		levelUpLabel.addLine("Your legacies have levelled up!");
-		levelUpLabel.addLine("Select a legacy");
 		this.labelList.add(levelUpLabel);
 		
+		levelUpLabel2 = new GuiLabel(fontRenderer, 1, this.width / 2 - 30, 10, 15, 75, 0xFFFFFFFF);
+		levelUpLabel2.addLine("Select a legacy:");
+		this.labelList.add(levelUpLabel2);
 		
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < LegacyLevels.legacies.length; i++)
 		{
-			this.buttonList.add(new GuiButton(i+1, this.width / 20, 60 + i*25, "Legacy " + (i+1)));
+			this.buttonList.add(new GuiButton(i+1, this.width / 20, 60 + i*25, "Legacy " + LegacyLevels.legacies[i].legacyName));
 		}
 		
 	}
@@ -39,7 +45,7 @@ public class LegacyGui extends GuiScreen
 	@Override
     protected void actionPerformed(GuiButton button) throws IOException
 	{
-        
+		legacyIndex = button.id - 1;
     }
 
 	@Override
@@ -50,7 +56,11 @@ public class LegacyGui extends GuiScreen
         this.drawBackground(0);
         
         super.drawScreen(mouseX, mouseY, partialTicks);
-        this.drawRect((int) (230), 60, this.width - 30, this.height - 30, 0xFFFFFFFF);
+        
+        this.drawRect(this.width - 170, 60, this.width - 30, this.height - 30, 0xFFFFFFFF);
+        this.drawCenteredString(this.fontRenderer, LegacyLevels.legacies[legacyIndex].legacyName, ((this.width - 170) + (this.width - 30)) / 2, 70, 0x0313fc);
+        
+        this.drawString(this.fontRenderer, LegacyLevels.legacies[legacyIndex].levelDescription, this.width - 160, 90, 0x0313fc);
         
     }
 
--- java/lorien/legacies/legacies/Legacy.java
+++ java/lorien/legacies/legacies/Legacy.java
@@ -16,6 +16,10 @@ public abstract class Legacy {
 	
 	protected boolean toggled = false;
 	
+	//protected static boolean HAS_LEVELS;
+	
+	
+	
 	// Called every tick
 	public abstract void computeLegacyTick(EntityPlayer player);
 	
