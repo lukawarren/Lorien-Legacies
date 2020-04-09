8102333def52614114c1bfeb30ed14a583869fe4
Luka Warren <lukawarren@icloud.com>
2020-04-09 11:43:38 +0100

    Fixed git issue

--- java/lorien/legacies/gui/StaminaGui.java
+++ java/lorien/legacies/gui/StaminaGui.java
@@ -60,52 +60,6 @@ public class StaminaGui extends GuiScreen // Extending for the drawTexturedModal
 	
     public void render(float percentage, int stamina, int maxStamina, RenderGameOverlayEvent event)
 	{
-<<<<<<< Updated upstream
-		
-    	ScaledResolution scaled = new ScaledResolution(Minecraft.getMinecraft());
-    	int screenWidth = scaled.getScaledWidth();
-    	int screenHeight = scaled.getScaledHeight();
-    	
-		// Calculate stamina percentage
-        if (percentage > 1.0f) percentage = 1.0f;
-        if (percentage < 0.0f) percentage = 0.0f;
-        
-        // Draw stamina bar (actually two bars - one background and one progress bar)
-        float padding = 2;
-        float width = Minecraft.getMinecraft().fontRenderer.getStringWidth("Stamina: " + maxStamina + " / " + maxStamina); // Expand bar to fill maximum width the text will ever need
-        float height = 20;
-        float x = screenWidth - width - padding;
-        float y = screenHeight - height - padding;
-        float widthPosMultipler = 200 / width;
-        float heightPosMultiplier = 20 / height;
-        
-        Minecraft.getMinecraft().getTextureManager().bindTexture(BUTTON_TEXTURES);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        GlStateManager.scale(width / 200, height / 20, 1.0f);
-        this.drawTexturedModalRect(x * widthPosMultipler, y * heightPosMultiplier, 0, 46 + 0 * 20, 200, 20);
-        GlStateManager.scale(200 / width, 20 / height, 1.0f);
-        
-        // Second bar
-        width *= percentage;
-        if (width < 0.001f) width = 0.001f; // width must be greater than 0 to avoid breaking the GL state
-        widthPosMultipler = 200 / width;
-        Minecraft.getMinecraft().getTextureManager().bindTexture(BUTTON_TEXTURES);
-        GlStateManager.color(0.0F, 0.0F, 1.0F, 1.0F);
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        GlStateManager.scale(width / 200, height / 20, 1.0f);
-        this.drawTexturedModalRect(x * widthPosMultipler, y * heightPosMultiplier, 0, 46 + 2 * 20, 200, 20);
-        GlStateManager.scale(200 / width, 20 / height, 1.0f);
-        
-        // Draw label
-        Minecraft.getMinecraft().fontRenderer.drawString("Stamina: " + (stamina) + " / " + maxStamina, (int)x, (int)y - 10, 0x0313fc);
-        
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-=======
 	
     	// If we're rendering the XP bar, intercept the event
     	if (event.getType() == ElementType.EXPERIENCE && !Minecraft.getMinecraft().player.isRidingHorse())
@@ -142,7 +96,6 @@ public class StaminaGui extends GuiScreen // Extending for the drawTexturedModal
             Minecraft.getMinecraft().getTextureManager().bindTexture(Gui.ICONS); // Fix hunger by binding its texture again
     		
     	}
->>>>>>> Stashed changes
     }
 	
 }
