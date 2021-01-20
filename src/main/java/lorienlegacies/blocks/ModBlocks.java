4186071ca200af49312236a23b13c704c182f515
Luka Warren <lukawarren@icloud.com>
2021-01-20 13:46:40 +0000

    Update ModBlocks.java

--- src/main/java/lorienlegacies/blocks/ModBlocks.java
+++ src/main/java/lorienlegacies/blocks/ModBlocks.java
@@ -1,13 +1,7 @@
 package lorienlegacies.blocks;
 
-import lorienlegacies.proxy.ClientProxy;
-import lorienlegacies.proxy.CommonProxy;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModBlocks
