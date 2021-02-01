c73102916d8247c7a8b7ec7ca3bc8be32666d4ed
5 <76455053+walter-afk@users.noreply.github.com>
2021-02-01 16:04:58 -0500

    Updated for 1.16.4 and the reworked Item Tiers.

--- src/main/java/lorienlegacies/items/ItemLeatherKnife.java
+++ src/main/java/lorienlegacies/items/ItemLeatherKnife.java
@@ -1,16 +1,16 @@
 package lorienlegacies.items;
 
 import lorienlegacies.core.LorienLegacies;
-import.lorienlegacies.items.ToolMaterialLoricMetal;
 
-import net.minecraft.recipe.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemSword;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ItemStack;
 
-public class ItemLeatherKnife extends ItemSword
+public class ItemLeatherKnife extends SwordItem
 {
-	public ItemLeatherKnife(ToolMaterial toolMat)
+	public ItemLeatherKnife(final IItemTier toolMat)
 	{
 		//super constructor params: material, additionally damage (zero index), attack speed (always negative, closer to 1 = faster), and creative menu location.
 		super(toolMat, -1, -2.2f, new Item.Settings().group(LorienLegacies.creativeTab));
